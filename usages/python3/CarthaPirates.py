cfdf82a4574aa1ba359255aa18b5ff863e485e4a
AdrienVH <adrien.van.hamme@gmail.com>
2021-11-26 14:18:39 +0100

    Correction des appels à l'API : modification des URLs sites/moyens en ports/bateaux

--- usages/python3/CarthaPirates.py
+++ usages/python3/CarthaPirates.py
@@ -4,22 +4,22 @@ class CarthaPirates(object):
     self.monBateau = idBateau
     self.url = "http://api.carthapirates.adrienvh.fr"
   def listerTousLesPorts(self):
-    response = requests.get(self.url + "/sites")
+    response = requests.get(self.url + "/ports")
     return response.json()
   def deplacerMonBateauVersCoords(self, coords):
     query = {"longitude":coords[0], "latitude":coords[1]}
-    requests.put(self.url + '/moyen/' + str(self.monBateau), params=query)
+    requests.put(self.url + '/bateaux/' + str(self.monBateau), params=query)
   def recupererCoordsPort(self, idPort):
-    response = requests.get(self.url + "/site/"+ str(idPort))
+    response = requests.get(self.url + "/ports/"+ str(idPort))
     return response.json()["geom"]["coordinates"]
   def recupererCoordsMonBateau(self):
-    response = requests.get(self.url + "/moyen/"+  str(self.monBateau))
+    response = requests.get(self.url + "/bateaux/"+  str(self.monBateau))
     return response.json()["geom"]["coordinates"]
   def trouverPortsProchesCoords(self, coords, nombre):
-    url = self.url + "/sites/nearest/" + str(nombre) + "/" + str(coords[0]) + "/" + str(coords[1])
+    url = self.url + "/ports/proches/" + str(nombre) + "/" + str(coords[0]) + "/" + str(coords[1])
     response = requests.get(url)
     return response.json()
   def trouverBateauxProchesCoords(self, coords, nombre):
-    url = self.url + "/moyens/nearest/" + str(nombre) + "/" + str(coords[0]) + "/" + str(coords[1])
+    url = self.url + "/bateaux/proches/" + str(nombre) + "/" + str(coords[0]) + "/" + str(coords[1])
     response = requests.get(url)
     return response.json()
